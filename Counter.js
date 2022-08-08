08b01998d35359e99541de45ca262d94b0ebef11
Florian Bär <florianbar01@gmail.com>
2022-08-08 10:11:20 +0200

    First commit

--- Counter.js
+++ Counter.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import { PropTypes } from 'prop-types';
+import React from "react";
+import { PropTypes } from "prop-types";
 
-const Counter = ({ value, onIncrement, onDecrement }) =>
-      <div>
-        <button onClick={onIncrement}>
-          Increment
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          Decrement
-        </button>
-        <hr />
-        <div>
-          Clicked: {value} times
-        </div>
-      </div>
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement,
+  onIncrementAsync,
+  onDecrementAsync,
+}) => (
+  <div>
+    <div>
+      <button onClick={onIncrement}>Increment</button>{" "}
+      <button onClick={onDecrement}>Decrement</button>
+    </div>
+    <div>
+      <button onClick={onIncrementAsync}>Increment after 1 second</button>{" "}
+      <button onClick={onDecrementAsync}>Decrement after 1 second</button>
+    </div>
+    <hr />
+    <div>Clicked: {value} times</div>
+  </div>
+);
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
   onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired
-}
+  onDecrement: PropTypes.func.isRequired,
+  onIncrementAsync: PropTypes.func.isRequired,
+  onDecrementAsync: PropTypes.func.isRequired,
+};
 
-export default Counter
+export default Counter;
